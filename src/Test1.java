a70a14fabeb03f5ed6a05125385316e2b1cb2e12
choiyounghun0712 <choiyounghun0712@gmail.com>
2023-04-06 16:30:58 +0900

    Test

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,20 @@
 
 public class Test1 {
-
+	public static void main(String[] args) {
+		System.out.println("***연산***");
+		
+		int num1=40;
+		int num2=20;
+		
+		//60 = 40 + 20
+		int addResult = num1 + num2;
+		System.out.println("num1+num2="+addResult);
+		int subResult=num1-num2;
+	    System.out.println("num1-num2="+subResult);
+	    int mulResult=num1*num2;
+	    System.out.println("num1*num2="+mulResult);
+	    int divResult=num1/num2;
+	    System.out.println("num1/num2="+divResult);
+	}
+	
 }
